fdf65415cac6b4efec91d033b1c8e0ef55ac30eb
Chris Dunphy <chris@megaparsec.ca>
2023-02-10 16:57:38 -0700

    Finished configuring Alacritty

--- home.nix
+++ home.nix
@@ -49,6 +49,7 @@
       sumneko-lua-language-server
       rnix-lsp
       ruff
+      rust-analyzer
 
       # null-ls sources
       black
@@ -71,6 +72,38 @@
       enable = true;
       interactiveShellInit = ''
         set fish_greeting # Disable greeting
+        set -l foreground cdcecf
+        set -l selection 223249
+        set -l comment 526176
+        set -l red c94f6d
+        set -l orange f4a261
+        set -l yellow dbc074
+        set -l green 81b29a
+        set -l purple 9d79d6
+        set -l cyan 63cdcf
+        set -l pink d67ad2
+
+        # Syntax Highlighting Colors
+        set -g fish_color_normal $foreground
+        set -g fish_color_command $cyan
+        set -g fish_color_keyword $pink
+        set -g fish_color_quote $yellow
+        set -g fish_color_redirection $foreground
+        set -g fish_color_end $orange
+        set -g fish_color_error $red
+        set -g fish_color_param $purple
+        set -g fish_color_comment $comment
+        set -g fish_color_selection --background=$selection
+        set -g fish_color_search_math --background=$selection
+        set -g fish_color_operator $green
+        set -g fish_color_escape $pink
+        set -g fish_color_autosuggestion $comment
+
+        # Completion Pager Colors
+        set -g fish_pager_color_progress $comment
+        set -g fish_pager_color_prefix $cyan
+        set -g fish_pager_color_completion $foreground
+        set -g fish_pager_color_description $commentc
       '';
 
       shellAliases = {
@@ -204,6 +237,56 @@
           style = "Medium";
         };
         size = 18;
+      };
+      cursor = {
+        style = {
+          shape = "Block";
+          blinking = "Always";
+        };
+      };
+      mouse_bindings = [
+        { mouse = "Right"; action = "Paste"; }
+        { mouse = "Left"; action = "Copy"; }
+      ];
+      window = {
+        decorations = "none";
+        padding = {
+          x = 3;
+          y = 3;
+        };
+        opacity = 0.97;
+      };
+      colors = {
+
+        primary = {
+          background = "0x192330";
+          foreground = "0xcdcecf";
+        };
+        normal = {
+          black = "0x393b44";
+          red = "0xc94f6d";
+          green = "0x81b29a";
+          yellow = "0xdbc074";
+          blue = "0x719cd6";
+          magenta = "0x9d79d6";
+          cyan = "0x63cdcf";
+          white = "0xdfdfe0";
+        };
+        # Bright colors
+        bright = {
+          black = "0x575860";
+          red = "0xd16983";
+          green = "0x8ebaa4";
+          yellow = "0xe0c989";
+          blue = "0x86abdc";
+          magenta = "0xbaa1e2";
+          cyan = "0x7ad4d6";
+          white = "0xe4e4e5";
+        };
+        indexed_colors = [
+          { index = 16; color = "0xf4a261"; }
+          { index = 17; color = "0xd67ad2"; }
+        ];
       };
     };
   };
